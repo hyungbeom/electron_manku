230858dec0bb362e4dca6e69d94b7af2252686a2
beomlee <lhbhb21c@gmail.com>
2024-10-21 14:13:07 +0900

    ㅁㄴㅇㄹ

---
+++ main/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,  // nodeIntegration을 사용해야 한다면 true로 설정
+        },
+    });
+
+    win.loadURL('http://192.168.219.102:3000'); // 올바른 URL 확인
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
