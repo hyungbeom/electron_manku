729257798e76b81e66f8f7e94e16575ae11ec066
beomlee <lhbhb21c@gmail.com>
2024-11-06 04:53:00 +0900

    컬럼오류 수정

--- src/pages/maker_read.tsx
+++ src/pages/maker_read.tsx
@@ -1,34 +1,51 @@
 import React, {useEffect, useRef, useState} from "react";
-import Input from "antd/lib/input/Input";
-import Select from "antd/lib/select";
-import LayoutComponent from "@/component/LayoutComponent";
-import Card from "antd/lib/card/Card";
-import {CopyOutlined, FileExcelOutlined, SearchOutlined} from "@ant-design/icons";
-import Button from "antd/lib/button";
-import {rfqReadColumns} from "@/utils/columnList";
-import DatePicker from "antd/lib/date-picker";
-import {subRfqReadInitial} from "@/utils/initialList";
+import moment from "moment/moment";
+import {getData} from "@/manage/function/api";
 import {wrapper} from "@/store/store";
 import initialServerRouter from "@/manage/function/initialServerRouter";
 import {setUserInfo} from "@/store/user/userSlice";
-import {getData} from "@/manage/function/api";
-import moment from "moment";
+import LayoutComponent from "@/component/LayoutComponent";
+import Card from "antd/lib/card/Card";
+import Input from "antd/lib/input/Input";
+
+import Button from "antd/lib/button";
+import {CopyOutlined, FileExcelOutlined, SearchOutlined,} from "@ant-design/icons";
 import * as XLSX from "xlsx";
-import TableGrid from "@/component/tableGrid";
 import message from "antd/lib/message";
 
-const {RangePicker} = DatePicker
+import {
+    makerColumn,
+    rfqReadColumns,
+    tableCodeDomesticPurchaseColumns,
+    tableCodeDomesticSalesColumns,
+    tableCodeOverseasPurchaseColumns,
+} from "@/utils/columnList";
+import {
+    codeDomesticPurchaseInitial,
+    tableCodeDomesticSalesInitial,
+    tableCodeOverseasSalesInitial,
+} from "@/utils/initialList";
+import Radio from "antd/lib/radio";
+import TableGrid from "@/component/tableGrid";
+import Search from "antd/lib/input/Search";
 
 
-export default function MakerRead({dataList}) {
+const TwinInputBox = ({children}) => {
+    return <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr', gridColumnGap: 5, paddingTop: 8}}>
+        {children}
+    </div>
+}
+export default function makerRead({dataList}) {
     const gridRef = useRef(null);
-    const [selectedRows, setSelectedRows] = useState([]);
-    const {estimateRequestList, pageInfo} = dataList;
-    const [info, setInfo] = useState(subRfqReadInitial);
-    const [tableData, setTableData] = useState(estimateRequestList);
+    let checkList = []
 
+    const {makerList, pageInfo} = dataList;
+    const [saveInfo, setSaveInfo] = useState(codeDomesticPurchaseInitial);
+    const [info, setInfo] = useState(codeDomesticPurchaseInitial);
+    const [tableData, setTableData] = useState(makerList);
 
-    // console.log(selectedRows, 'selectedRows')
+    const [paginationInfo, setPaginationInfo] = useState(pageInfo);
+
 
 
     function onChange(e) {
@@ -41,107 +58,69 @@ export default function MakerRead({dataList}) {
         })
     }
 
-    useEffect(() => {
-        const copyData: any = {...info}
-        copyData['searchDate'] = [moment().subtract(1, 'years').format('YYYY-MM-DD'), moment().format('YYYY-MM-DD')];
-        setInfo(copyData);
-        // setTableInfo(transformData(estimateRequestList, 'estimateRequestId', 'estimateRequestDetailList'));
-        // setTableData(estimateRequestList);
-        // console.log(tableData, 'setTableData')
-    }, [])
-
-
-    async function searchInfo() {
-        const copyData: any = {...info}
-        const {searchDate}: any = copyData;
-        if (searchDate) {
-            copyData['searchStartDate'] = searchDate[0];
-            copyData['searchEndDate'] = searchDate[1];
+    async function onSearch() {
+        const result = await getData.post('maker/getMakerList', info);
+        console.log(result?.data?.entity?.makerList,'result:')
+        if(result?.data?.code === 1){
+            setTableData(result?.data?.entity?.makerList)
         }
-        console.log(copyData,'copyData:')
-        const result = await getData.post('estimate/getEstimateRequestList', {...copyData,   "page": 1,
-            "limit": -1});
-        // setTableInfo(transformData(result?.data?.entity?.estimateRequestList, 'estimateRequestId', 'estimateRequestDetailList'));
-        console.log(result?.data?.entity?.estimateRequestList,'result?.data?.entity?.estimateRequestList:')
-        setTableData(result?.data?.entity?.estimateRequestList);
     }
 
+    return <LayoutComponent>
+        <div style={{display: 'grid', gridTemplateRows: '120px 1fr', height: '100%', gridColumnGap: 5}}>
+            <Card size={'small'} title={'국내대리점관리(매입)'} style={{fontSize: 12, border: '1px solid lightGray'}}>
+                <Card size={'small'} style={{
+                    fontSize: 13,
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.02), 0 6px 20px rgba(0, 0, 0, 0.02)'
+                }}>
+                    <div style={{display: 'grid', gridTemplateColumns: '300px 1fr'}}>
+                        <div>
+                            <Radio.Group onChange={e=> setInfo(v=>{return {...v, searchType: e.target.value}})} defaultValue={2} id={'searchType'}
+                                         value={info['searchType']}>
+                                <Radio value={1}>코드</Radio>
+                                <Radio value={2}>상호명</Radio>
+                                <Radio value={3}>지역</Radio>
+                                <Radio value={4}>전화번호</Radio>
+                            </Radio.Group>
+                        </div>
 
-    function deleteList(checkList) {
-        const api = gridRef.current.api;
-        console.log(api.getSelectedRows(),':::')
-    }
+                        <Search
+                            onSearch={onSearch}
+                            onChange={onChange}
+                            id={'searchText'}
+                            placeholder="input search text"
+                            allowClear
+                            enterButton="검색"
+                            size="small"
+                            // onSearch={onSearch}
+                        />
 
+                    </div>
 
-
-    async function getDetailData(params) {
-        const result = await getData.post('estimate/getEstimateRequestDetail', {
-            estimateRequestId:params
-        });
-        setTableData(result?.data?.entity?.estimateRequestList)
-    }
-
-    const downloadExcel = () => {
-
-        const worksheet = XLSX.utils.json_to_sheet(tableData);
-        const workbook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-        XLSX.writeFile(workbook, "example.xlsx");
-    };
-
-    const rowSelection = {
-        onChange: (selectedRowKeys, selectedRows) => {
-            selectedRows = selectedRowKeys
-        },
-        onDoubleClick: (src) => {
-            console.log(src, ':::')
-        },
-        getCheckboxProps: (record) => ({
-            disabled: record?.name === 'Disabled User',
-            // Column configuration not to be checked
-            name: record?.name,
-        }),
-    };
-
-    return <>
-        <LayoutComponent>
-            <div style={{display: 'grid', gridTemplateColumns: '350px 1fr', height: '100%', gridColumnGap: 5}}>
-                <Card title={'의뢰 조회'} style={{fontSize: 12, border: '1px solid lightGray'}}>
-                    <Card size={'small'} style={{
-                        fontSize: 13,
-                        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.02), 0 6px 20px rgba(0, 0, 0, 0.02)'
-                    }}>
-                        ㄴㅇㄹ
-                    </Card>
                 </Card>
+            </Card>
 
-                <TableGrid
-                    gridRef={gridRef}
-                    columns={rfqReadColumns}
-                    tableData={tableData}
-                    setSelectedRows={setSelectedRows}
-                    // dataInfo={tableOrderReadInfo}
-                    // setDatabase={setInfo}
-                    // setTableInfo={setTableData}
-                    type={'read'}
-                    excel={true}
-                    funcButtons={<div><Button type={'primary'} size={'small'} style={{fontSize: 11}}>
-                        <CopyOutlined/>복사
-                    </Button>
-                        {/*@ts-ignored*/}
-                        <Button type={'danger'} size={'small'} style={{fontSize: 11, marginLeft:5,}} onClick={deleteList}>
-                            <CopyOutlined/>삭제
-                        </Button>
-                        <Button type={'dashed'} size={'small'} style={{fontSize: 11, marginLeft:5,}} onClick={downloadExcel}>
-                            <FileExcelOutlined/>출력
-                        </Button></div>}
-                />
+            <TableGrid
+                gridRef={gridRef}
+                columns={makerColumn}
+                tableData={tableData}
+                type={'read'}
+                excel={true}
+                // funcButtons={<div><Button type={'primary'} size={'small'} style={{fontSize: 11}}>
+                //     <CopyOutlined/>복사
+                // </Button>
+                //     {/*@ts-ignored*/}
+                //     <Button type={'danger'} size={'small'} style={{fontSize: 11, marginLeft:5,}} onClick={deleteList}>
+                //         <CopyOutlined/>삭제
+                //     </Button>
+                //     <Button type={'dashed'} size={'small'} style={{fontSize: 11, marginLeft:5,}} onClick={downloadExcel}>
+                //         <FileExcelOutlined/>출력
+                //     </Button></div>}
+            />
 
-            </div>
-        </LayoutComponent>
-    </>
+        </div>
+    </LayoutComponent>
 }
-
 
 // @ts-ignore
 export const getServerSideProps = wrapper.getStaticProps((store: any) => async (ctx: any) => {
@@ -149,39 +128,33 @@ export const getServerSideProps = wrapper.getStaticProps((store: any) => async (
 
     let param = {}
 
-    const {userInfo} = await initialServerRouter(ctx, store);
+    const {userInfo, codeInfo} = await initialServerRouter(ctx, store);
 
-    if (!userInfo) {
-        return {
-            redirect: {
-                destination: '/', // 리다이렉트할 경로
-                permanent: false, // true면 301 리다이렉트, false면 302 리다이렉트
-            },
-        };
-    }
-
-    store.dispatch(setUserInfo(userInfo));
-
-    const result = await getData.post('estimate/getEstimateRequestList', {
-        "searchEstimateRequestId": "",      // 견적의뢰 Id
-        "searchType": "",                   // 검색조건 1: 회신, 2: 미회신
-        "searchStartDate": moment().subtract(1, 'years').format('YYYY-MM-DD'),              // 작성일자 시작일
-        "searchEndDate": moment().format('YYYY-MM-DD'),                // 작성일자 종료일
-        "searchDocumentNumber": "",         // 문서번호
-        "searchCustomerName": "",           // 거래처명
-        "searchMaker": "",                  // MAKER
-        "searchModel": "",                  // MODEL
-        "searchItem": "",                   // ITEM
-        "searchCreatedBy": "",              // 등록직원명
-        "searchManagerName": "",            // 담당자명
-        "searchMobileNumber": "",           // 담당자 연락처
-        "searchBiddingNumber": "",          // 입찰번호(미완성)
+    const result = await getData.post('maker/getMakerList', {
+        "searchType": "1",      // 1: 코드, 2: 상호명, 3: MAKER
+        "searchText": "",
         "page": 1,
         "limit": -1
     });
 
+    console.log(result?.data?.entity,'result?.data?.entity:')
 
-    return {
-        props: {dataList: result?.data?.entity}
+    if (userInfo) {
+        store.dispatch(setUserInfo(userInfo));
     }
+    if (codeInfo !== 1) {
+        param = {
+            redirect: {
+                destination: '/', // 리다이렉트할 대상 페이지
+                permanent: false, // true로 설정하면 301 영구 리다이렉트, false면 302 임시 리다이렉트
+            },
+        };
+    } else {
+        // result?.data?.entity?.estimateRequestList
+        param = {
+            props: {dataList: result?.data?.entity}
+        }
+    }
+
+    return param
 })
--- src/pages/order_read_customer.tsx
+++ src/pages/order_read_customer.tsx
@@ -1,39 +1,53 @@
-import React, {useEffect, useState} from "react";
-import Input from "antd/lib/input/Input";
-import LayoutComponent from "@/component/LayoutComponent";
-import CustomTable from "@/component/CustomTable";
-import Card from "antd/lib/card/Card";
-import {FileExcelOutlined, SearchOutlined} from "@ant-design/icons";
-import Button from "antd/lib/button";
-import {tableOrderCustomerColumns} from "@/utils/columnList";
-import DatePicker from "antd/lib/date-picker";
-import {orderCustomerReadInitial, tableOrderCustomerInitial} from "@/utils/initialList";
-import {tableOrderCustomerInfo} from "@/utils/modalDataList";
+import React, {useEffect, useRef, useState} from "react";
+import moment from "moment/moment";
+import {getData} from "@/manage/function/api";
 import {wrapper} from "@/store/store";
 import initialServerRouter from "@/manage/function/initialServerRouter";
 import {setUserInfo} from "@/store/user/userSlice";
-import {getData} from "@/manage/function/api";
-import moment from "moment";
-import * as XLSX from "xlsx";
-import dayjs from 'dayjs';
+import LayoutComponent from "@/component/LayoutComponent";
+import Card from "antd/lib/card/Card";
+import Input from "antd/lib/input/Input";
 
-const {RangePicker} = DatePicker
+import Button from "antd/lib/button";
+import {CopyOutlined, FileExcelOutlined, SearchOutlined,} from "@ant-design/icons";
+import * as XLSX from "xlsx";
+import message from "antd/lib/message";
+
+import {
+    rfqReadColumns,
+    tableCodeDomesticPurchaseColumns,
+    tableCodeDomesticSalesColumns,
+    tableCodeOverseasPurchaseColumns, tableOrderCustomerColumns,
+} from "@/utils/columnList";
+import {
+    codeDomesticPurchaseInitial, orderCustomerReadInitial,
+    tableCodeDomesticSalesInitial,
+    tableCodeOverseasSalesInitial,
+} from "@/utils/initialList";
+import Radio from "antd/lib/radio";
+import TableGrid from "@/component/tableGrid";
+import Search from "antd/lib/input/Search";
+
 
 const TwinInputBox = ({children}) => {
     return <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr', gridColumnGap: 5, paddingTop: 8}}>
         {children}
     </div>
 }
-
-export default function OrderReadCustomer({dataList}) {
+export default function orderReadCustomer({dataList}) {
+    const gridRef = useRef(null);
     let checkList = []
 
     const {orderList, pageInfo} = dataList;
-    const [info, setInfo] = useState(orderCustomerReadInitial)
-    const [tableInfo, setTableInfo] = useState(orderList)
-    const [paginationInfo, setPaginationInfo] = useState(pageInfo)
+    const [saveInfo, setSaveInfo] = useState(orderCustomerReadInitial);
+    const [info, setInfo] = useState(orderCustomerReadInitial);
+    const [tableData, setTableData] = useState(orderList);
 
-    // console.log(pageInfo,'pageInfo:')
+    const [paginationInfo, setPaginationInfo] = useState(pageInfo);
+
+    console.log(orderList,'saveInfo:')
+
+
     function onChange(e) {
 
         let bowl = {}
@@ -44,112 +58,68 @@ export default function OrderReadCustomer({dataList}) {
         })
     }
 
-    useEffect(() => {
-        const copyData: any = {...info}
-        copyData['searchDate'] = [moment().format('YYYY-MM-DD'), moment().format('YYYY-MM-DD')];
-        setInfo(copyData);
-        // setTableInfo(transformData(orderList, 'customerName', 'orderList'));
-    }, [])
-
-
-
-    async function searchInfo() {
-        const copyData: any = {...info}
-        const {searchDate}: any = copyData;
-        if (searchDate) {
-            copyData['searchStartDate'] = searchDate[0];
-            copyData['searchEndDate'] = searchDate[1];
+    async function onSearch() {
+        const result = await getData.post('settlement/getOrderListByCustomer', info);
+        if(result?.data?.code === 1){
+            setTableData(result?.data?.entity?.orderList)
         }
-        console.log(searchDate, 'searchDate')
-
-        const result = await getData.post('settlement/getOrderListByCustomer', copyData);
-        console.log(result?.data?.entity, 'result')
-        // setTableInfo(transformData(result?.data?.entity?.inventoryList, 'inventoryId', 'inventoryList'));
-        setTableInfo(result?.data?.entity?.orderList);
-        setPaginationInfo(result?.data?.entity?.pageInfo)
     }
 
-
-    const downloadExcel = () => {
-
-        const worksheet = XLSX.utils.json_to_sheet(tableInfo);
-        const workbook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-        XLSX.writeFile(workbook, "example.xlsx");
-    };
-
-    const rowSelection = {
-        onChange: (selectedRowKeys, selectedRows) => {
-
-            checkList  = selectedRowKeys
-
-        },
-        getCheckboxProps: (record) => ({
-            disabled: record.name === 'Disabled User',
-            // Column configuration not to be checked
-            name: record.name,
-        }),
-    };
-
-
-    return <>
-        <LayoutComponent>
-            <div style={{display: 'grid', gridTemplateColumns: '350px 1fr', height: '100%', gridColumnGap: 5}}>
-                <Card title={'거래처 별 주문 조회'} style={{fontSize: 12, border: '1px solid lightGray'}}>
-                    <Card size={'small'} style={{
-                        fontSize: 13,
-                        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.02), 0 6px 20px rgba(0, 0, 0, 0.02)'
-                    }}>
+    return <LayoutComponent>
+        <div style={{display: 'grid', gridTemplateRows: '120px 1fr', height: '100%', gridColumnGap: 5}}>
+            <Card size={'small'} title={'국내대리점관리(매입)'} style={{fontSize: 12, border: '1px solid lightGray'}}>
+                <Card size={'small'} style={{
+                    fontSize: 13,
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.02), 0 6px 20px rgba(0, 0, 0, 0.02)'
+                }}>
+                    <div style={{display: 'grid', gridTemplateColumns: '300px 1fr'}}>
                         <div>
-                            <div style={{paddingBottom: 3}}>조회일자</div>
-                            {/*@ts-ignore*/}
-                            <RangePicker style={{width: '100%'}}
-                                         value={[moment(info['searchDate'][0]), moment(info['searchDate'][1])]}
-                                         id={'searchDate'} size={'small'} onChange={(date, dateString) => {
-                                onChange({
-                                    target: {
-                                        id: 'searchDate',
-                                        value: date ? [moment(date[0]).format('YYYY-MM-DD'), moment(date[1]).format('YYYY-MM-DD')] : [moment().format('YYYY-MM-DD'), moment().format('YYYY-MM-DD')]
-                                    }
-                                })
-                            }
-                            }/>
-                        </div>
-                        <div style={{marginTop:8}}>
-                                <div style={{paddingBottom: 3}}>거래처명</div>
-                                <Input id={'searchCustomerName'} value={info['searchCustomerName']} onChange={onChange} size={'small'}/>
+                            <Radio.Group onChange={e=> setInfo(v=>{return {...v, searchType: e.target.value}})} defaultValue={2} id={'searchType'}
+                                         value={info['searchType']}>
+                                <Radio value={1}>코드</Radio>
+                                <Radio value={2}>상호명</Radio>
+                                <Radio value={3}>지역</Radio>
+                                <Radio value={4}>전화번호</Radio>
+                            </Radio.Group>
                         </div>
 
+                        <Search
+                            onSearch={onSearch}
+                            onChange={onChange}
+                            id={'searchText'}
+                            placeholder="input search text"
+                            allowClear
+                            enterButton="검색"
+                            size="small"
+                            // onSearch={onSearch}
+                        />
 
-                    </Card>
-                    <div style={{paddingTop: 20, textAlign: 'right'}}>
-                        <Button type={'primary'} style={{marginRight: 8}}
-                                onClick={searchInfo}><SearchOutlined/>조회</Button>
                     </div>
+
                 </Card>
+            </Card>
 
-                <CustomTable columns={tableOrderCustomerColumns}
-                             initial={tableOrderCustomerInitial}
-                             dataInfo={tableOrderCustomerInfo}
-                             info={tableInfo}
-                             setDatabase={setInfo}
-                             setTableInfo={setTableInfo}
-                             rowSelection={rowSelection}
-                             pageInfo={paginationInfo}
-                             visible={true}
-                             setPaginationInfo={setPaginationInfo}
+            <TableGrid
+                gridRef={gridRef}
+                columns={tableOrderCustomerColumns}
+                tableData={tableData}
+                type={'read'}
+                excel={true}
+                // funcButtons={<div><Button type={'primary'} size={'small'} style={{fontSize: 11}}>
+                //     <CopyOutlined/>복사
+                // </Button>
+                //     {/*@ts-ignored*/}
+                //     <Button type={'danger'} size={'small'} style={{fontSize: 11, marginLeft:5,}} onClick={deleteList}>
+                //         <CopyOutlined/>삭제
+                //     </Button>
+                //     <Button type={'dashed'} size={'small'} style={{fontSize: 11, marginLeft:5,}} onClick={downloadExcel}>
+                //         <FileExcelOutlined/>출력
+                //     </Button></div>}
+            />
 
-                             subContent={<>
-                                 <Button type={'dashed'} size={'small'} style={{fontSize: 11, margin:'0 0 0 auto'}} onClick={downloadExcel}>
-                                     <FileExcelOutlined/>출력
-                                 </Button></>}
-                />
-
-            </div>
-        </LayoutComponent>
-    </>
+        </div>
+    </LayoutComponent>
 }
-
 
 // @ts-ignore
 export const getServerSideProps = wrapper.getStaticProps((store: any) => async (ctx: any) => {
@@ -160,13 +130,13 @@ export const getServerSideProps = wrapper.getStaticProps((store: any) => async (
     const {userInfo, codeInfo} = await initialServerRouter(ctx, store);
 
     const result = await getData.post('settlement/getOrderListByCustomer', {
-        "searchStartDate": "",      // 조회일자 시작일
-        "searchEndDate": "",        // 조회일자 종료일
-        "searchCustomerName": "",   // 거래처명
+        "searchType": "1",      // 1: 코드, 2: 상호명, 3: MAKER
+        "searchText": "",
         "page": 1,
-        "limit": 10
+        "limit": -1
     });
 
+    console.log(result?.data?.entity,'result?.data?.entity:')
 
     if (userInfo) {
         store.dispatch(setUserInfo(userInfo));
