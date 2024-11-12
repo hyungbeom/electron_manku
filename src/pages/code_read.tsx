be8d9d08ea0208a751c5ff716dae39fcfb7259f6
Gina <kjh@progist.co.kr>
2024-11-12 18:28:26 +0900

    [update] HS코드 관리 수정중

--- src/pages/code_read.tsx
+++ src/pages/code_read.tsx
@@ -9,7 +9,14 @@ import Card from "antd/lib/card/Card";
 import Input from "antd/lib/input/Input";
 
 import Button from "antd/lib/button";
-import {CopyOutlined, FileExcelOutlined, SearchOutlined,} from "@ant-design/icons";
+import {
+    CopyOutlined,
+    DownCircleFilled,
+    FileExcelOutlined,
+    SaveOutlined,
+    SearchOutlined,
+    UpCircleFilled,
+} from "@ant-design/icons";
 import * as XLSX from "xlsx";
 import message from "antd/lib/message";
 
@@ -29,21 +36,14 @@ import TableGrid from "@/component/tableGrid";
 import Search from "antd/lib/input/Search";
 
 
-const TwinInputBox = ({children}) => {
-    return <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr', gridColumnGap: 5, paddingTop: 8}}>
-        {children}
-    </div>
-}
 export default function codeRead({dataList}) {
     const gridRef = useRef(null);
-    let checkList = []
+    const [mini, setMini] = useState(true);
 
-    const {hsCodeList, pageInfo} = dataList;
+    const {hsCodeList, } = dataList;
     const [saveInfo, setSaveInfo] = useState(codeReadInitial);
     const [info, setInfo] = useState(codeReadInitial);
     const [tableData, setTableData] = useState(hsCodeList);
-
-    const [paginationInfo, setPaginationInfo] = useState(pageInfo);
 
     console.log(hsCodeList,'saveInfo:')
 
@@ -66,22 +66,79 @@ export default function codeRead({dataList}) {
         }
     }
 
+    function deleteList() {
+
+        const api = gridRef.current.api;
+
+        // 전체 행 반복하면서 선택되지 않은 행만 추출
+        const uncheckedData = [];
+        for (let i = 0; i < api.getDisplayedRowCount(); i++) {
+            const rowNode = api.getDisplayedRowAtIndex(i);
+            if (!rowNode.isSelected()) {
+                uncheckedData.push(rowNode.data);
+            }
+        }
+
+        let copyData = {...info}
+        copyData['overseasCustomerManagerList'] = uncheckedData;
+        console.log(copyData, 'copyData::')
+        setInfo(copyData);
+
+    }
+
+
+    function addRow() {
+        let copyData = {...info};
+        copyData['overseasCustomerManagerList'].push({
+            "managerName": "",       // 담당자명
+            "directTel": "",     // 직통전화
+            "faxNumber": "",   // 팩스번호
+            "mobileNumber": "", // 휴대폰번호
+            "email": "",        // 이메일
+            "remarks": ""
+        })
+
+        setInfo(copyData)
+    }
+
+
     return <LayoutComponent>
-        <div style={{display: 'grid', gridTemplateRows: '120px 1fr', height: '100%', gridColumnGap: 5}}>
-            <Card size={'small'} title={'국내대리점관리(매입)'} style={{fontSize: 12, border: '1px solid lightGray'}}>
-                <Card size={'small'} style={{
-                    fontSize: 13,
-                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.02), 0 6px 20px rgba(0, 0, 0, 0.02)'
-                }}>
-                    <div style={{display: 'grid', gridTemplateColumns: '300px 1fr'}}>
+        <div
+            style={{display: 'grid', gridTemplateRows: `${mini ? 'auto' : '65px'} 1fr`, height: '100%', columnGap: 5,}}>
+            <Card title={'해외 거래처 등록'} style={{fontSize: 12, border: '1px solid lightGray'}}
+                  extra={<span style={{fontSize: 20, cursor: 'pointer'}} onClick={() => setMini(v => !v)}> {!mini ?
+                      <UpCircleFilled/> : <DownCircleFilled/>}</span>}>
+                {mini ? <>
+                    <div style={{display: 'grid', gridTemplateColumns: '0.5fr 1.5fr 1.5fr 1.5fr', columnGap: 20}}>
+                        <Card size={'small'}
+                              style={{
+                                  fontSize: 13,
+                                  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.02), 0 6px 20px rgba(0, 0, 0, 0.02)'
+                              }}>
+
+                            <div>
+                                <div style={{paddingTop: 8}}>ITEM</div>
+                                <Input id={'phoneNumber'} value={info['phoneNumber']} onChange={onChange}
+                                       size={'small'}/>
+                            </div>
+                            <div>
+                                <div style={{paddingTop: 8}}>HS-CODE</div>
+                                <Input id={'faxNumber'} value={info['faxNumber']} onChange={onChange}
+                                       size={'small'}/>
+                            </div>
+
+                        </Card>
                         <div>
-                            <Radio.Group onChange={e=> setInfo(v=>{return {...v, searchType: e.target.value}})} defaultValue={2} id={'searchType'}
-                                         value={info['searchType']}>
-                                <Radio value={1}>코드</Radio>
-                                <Radio value={2}>상호명</Radio>
-                                <Radio value={3}>지역</Radio>
-                                <Radio value={4}>전화번호</Radio>
-                            </Radio.Group>
+                            {/*@ts-ignored*/}
+                            <Button type={'primary'} size={'small'} style={{fontSize: 11, marginLeft: 5,}}
+                                    onClick={addRow}>
+                                <SaveOutlined/>추가
+                            </Button>
+                            {/*@ts-ignored*/}
+                            <Button type={'danger'} size={'small'} style={{fontSize: 11, marginLeft: 5,}}
+                                    onClick={deleteList}>
+                                <CopyOutlined/>삭제
+                            </Button>
                         </div>
 
                         <Search
@@ -90,14 +147,11 @@ export default function codeRead({dataList}) {
                             id={'searchText'}
                             placeholder="input search text"
                             allowClear
-                            enterButton="검색"
-                            size="small"
-                            // onSearch={onSearch}
+                            enterButton={<><SearchOutlined/>&nbsp;&nbsp; 조회</>}
                         />
 
                     </div>
-
-                </Card>
+                </> : null}
             </Card>
 
             <TableGrid
