2d574aa40dea16458c5fea298c3055ab69ccee28
beomlee <lhbhb21c@gmail.com>
2024-12-03 03:02:05 +0900

    견적의뢰 부분 전면 수정

--- src/pages/rfq_write.tsx
+++ src/pages/rfq_write.tsx
@@ -270,7 +270,18 @@ export default function rqfWrite() {
 
     function addRow() {
         let copyData = {...info};
-        copyData['estimateRequestDetailList'].push(subRfqTableInitial)
+        copyData['estimateRequestDetailList'].push( {
+            "model": "",             // MODEL
+            "quantity": 0,           // 수량
+            "unit": "ea",            // 단위
+            "currency": "KRW",       // CURR
+            "net": 0,                // NET/P
+            "serialNumber": 0,       // 항목 순서 (1부터 시작)
+            "deliveryDate": "",      // 납기
+            "content": "미회신",       // 내용
+            "replyDate": null,      // 회신일
+            "remarks": ""            // 비고
+        })
         setInfo(copyData)
     }
 
