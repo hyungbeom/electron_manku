00998dd764a63593b2b7fae79d42417f867ad956
12gina <kjh@progist.co.kr>
2025-01-25 22:25:29 +0900

    랜딩페이지 헤더 수정

--- src/component/homepage/Header.tsx
+++ src/component/homepage/Header.tsx
@@ -1,104 +1,205 @@
-export default function Header(){
+import React, {useState} from "react";
+import {backgroundColor} from "html2canvas/dist/types/css/property-descriptors/background-color";
+import {useRouter} from "next/router";
 
-    return <>
-        <div style={{
-            position: 'fixed',
-            zIndex: 120,
-            top: 0,
-            left: 0,
-            minWidth: 1048,
-            width: '100%',
-            padding: '30px 35px 30px 35px',
-            color: 'black',
-            backgroundColor: 'white',
-            borderBottom: '1px solid lightGray'
-        }}>
-            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                <div style={{display: 'flex', alignItems: 'center', gap: '10px', fontWeight: 600}}><img
-                    src={'/homepage/logo_1.png'} alt=""/>Manku Trading
-                </div>
+export default function Header() {
 
-                <div style={{display: 'flex', gap: '30px', textAlign: 'center', fontWeight: 600}}>
-                    <span style={{cursor: "pointer", width: 150}}>기업정보</span>
-                    <span style={{cursor: "pointer", width: 150}}>사업분야</span>
-                    <span style={{cursor: "pointer", width: 150}}>한국대리점</span>
-                    <span style={{cursor: "pointer", width: 150}}>고객센터</span>
-                </div>
+    const router = useRouter();
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [hoverMenu, setHoverMenu] = useState(null);
 
-                <div style={{display: 'flex', gap: '25px'}}>
-                    <img src={'/homepage/search.svg'} alt=""/>
-                    <img src={'/homepage/lang.svg'} alt=""/>
-                    <span>KOR</span>
+    function enterMouse(e){
+        setHoverMenu(e.target.id)
+        setIsExpanded(true)
+    }
+    function outMouse(e){
+        setHoverMenu(null);
+        setIsExpanded(false)
+    }
+    return (
+        <>
+            {/* 상단 고정된 헤더 */}
+            <div
+                style={{
+                    position: "fixed",
+                    zIndex: 120,
+                    top: 0,
+                    left: 0,
+                    minWidth: 1048,
+                    width: "100%",
+                    // padding: "30px 35px",
+                    color: "black",
+                    backgroundColor: "white",
+
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        padding: '0px 40px'
+                    }}
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "10px",
+                            fontWeight: 600,
+                            cursor : 'pointer'
+                        }}  onClick={()=>router.push('/homepage')}>
+                        <img src={"/homepage/logo_1.png"}/>
+                        Manku Trading
+                    </div>
+
+                    <div style={{
+                        display: "flex",
+                        textAlign: "center",
+                        fontWeight: 600,
+                        alignItems: 'center'
+
+                    }} onPointerEnter={enterMouse}  onPointerLeave={outMouse}>
+                        <div style={{
+                            cursor: "pointer",
+                            width: 150,
+                            height: 83,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            color : hoverMenu === '1' ? '#173F95' : '',
+                            borderBottom: hoverMenu === '1' ? '2px solid #173F95' : ''
+                        }} id={'1'} onPointerEnter={enterMouse}>기업정보
+                        </div>
+                        <div style={{
+                            cursor: "pointer",
+                            width: 150,
+                            height: 83,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            color : hoverMenu === '2' ? '#173F95' : '',
+                            borderBottom: hoverMenu === '2' ? '2px solid #173F95' : ''
+                        }} id={'2'} onPointerEnter={enterMouse}>사업분야
+                        </div>
+                        <div style={{
+                            cursor: "pointer",
+                            width: 150,
+                            height: 83,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            color : hoverMenu === '3' ? '#173F95' : '',
+                            borderBottom: hoverMenu === '3' ? '2px solid #173F95' : ''
+                        }} id={'3'} onPointerEnter={enterMouse}>한국대리점
+                        </div>
+                        <div style={{
+                            cursor: "pointer",
+                            width: 150,
+                            height: 83,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            color : hoverMenu === '4' ? '#173F95' : '',
+                            borderBottom: hoverMenu === '4' ? '2px solid #173F95' : ''
+                        }} id={'4'} onPointerEnter={enterMouse}>고객센터
+                        </div>
+                    </div>
+
+                    <div style={{display: "flex", gap: "25px"}}>
+                        <img src={"/homepage/search.svg"} alt=""/>
+                        <img src={"/homepage/lang.svg"} alt=""/>
+                        <span>KOR</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div style={{
-            position: 'fixed',
-            zIndex: 120,
-            top: 83,
-            left: 0,
-            minWidth: 1048,
-            width: '100%',
-            padding: '30px 75px 30px 75px',
-            color: 'black',
-            backgroundColor: 'white',
-            borderBottom: '1px solid lightGray'
-        }}>
-            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                <div style={{paddingLeft: 20}}/>
 
-                <div style={{display: 'flex', gap: '30px', textAlign: 'center'}}>
-                             <span style={{
-                                 cursor: "pointer",
-                                 width: 150,
-                                 display: 'grid',
-                                 gridTemplateRows: '35px 35px 35px 35px'
-                             }}>
-                                <div>회사소개</div>
-                                <div>연혁</div>
-                                <div>주요고객</div>
-                                <div>오시는길</div>
-                            </span>
-                    <span style={{
-                        opacity: 0.3,
-                        cursor: "pointer",
-                        width: 150,
-                        display: 'grid',
-                        gridTemplateRows: '35px 35px 35px 35px'
-                    }}>
-                                <div>MRO 서비스</div>
-                                <div>플랜트 사업부</div>
-                                <div>반도체 사업부</div>
-                                <div>무역 사업부</div>
-                            </span>
-                    <span style={{
-                        opacity: 0.3,
-                        cursor: "pointer",
-                        width: 150,
+            {/* 펼쳐지는 섹션 */}
+            <div
+                className={`expandable-section ${isExpanded ? "expanded" : ""}`}
+                style={{
+                    position: "fixed",
+                    zIndex: 120,
+                    top: 83,
+                    left: 0,
+                    minWidth: 1048,
+                    width: "100%",
+                    overflow: "hidden", // 숨김 처리
+                    borderTop : '0.1px solid lightGray'
+                }}>
+                <div
+                    style={{
+                        // padding: "30px 75px",
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
+                    <div style={{paddingLeft: 20}}/>
 
-                        display: 'grid',
-                        gridTemplateRows: '35px 35px 35px 35px'
-                    }}>
-                                <div>KOENIG</div>
-                                <div>DST Chemical</div>
-                                <div>Daiichi</div>
-                                <div>Powerflex</div>
-                                <div>Bamo</div>
-                            </span>
-                    <span style={{
-                        opacity: 0.3,
-                        cursor: "pointer",
-                        width: 150,
-
-                        display: 'grid',
-                        gridTemplateRows: '35px 35px 35px 35px'
-                    }}>
-                                <div>온라인문의</div>
-                                <div>채용안내</div>
-                            </span>
+                    <div style={{display: "flex",  textAlign: "center", padding : '20px 0px'}} onPointerEnter={() => setIsExpanded(true)} onPointerLeave={() => setIsExpanded(false)}>
+            <span
+                style={{
+                    cursor: "pointer",
+                    width: 150,
+                    display: "grid",
+                    gridTemplateRows: "35px 35px 35px 35px",
+                    color: hoverMenu === '1' ? 'black' : '#A5A5A5'
+                }}
+            onPointerEnter={()=>setHoverMenu('1')}>
+              <div  className="menu-item">회사소개</div>
+              <div className="menu-item">연혁</div>
+              <div className="menu-item">주요고객</div>
+              <div className="menu-item">오시는길</div>
+            </span>
+                        <span
+                            onPointerEnter={()=>setHoverMenu('2')}
+                            style={{
+                                cursor: "pointer",
+                                width: 150,
+                                display: "grid",
+                                gridTemplateRows: "35px 35px 35px 35px",
+                                color: hoverMenu === '2' ? 'black' : '#A5A5A5'
+                            }}
+                        >
+              <div className="menu-item">MRO 서비스</div>
+              <div className="menu-item">플랜트 사업부</div>
+              <div className="menu-item">반도체 사업부</div>
+              <div className="menu-item">무역 사업부</div>
+            </span>
+                        <span
+                            onPointerEnter={()=>setHoverMenu('3')}
+                            style={{
+                                cursor: "pointer",
+                                width: 150,
+                                display: "grid",
+                                gridTemplateRows: "35px 35px 35px 35px",
+                                color: hoverMenu === '3' ? 'black' : '#A5A5A5'
+                            }}
+                        >
+              <div className="menu-item">KOENIG</div>
+              <div className="menu-item">DST Chemical</div>
+              <div className="menu-item">Daiichi</div>
+              <div className="menu-item">Powerflex</div>
+              <div className="menu-item">Bamo</div>
+            </span>
+                        <span
+                        onPointerEnter={()=>setHoverMenu('4')}
+                            style={{
+                                cursor: "pointer",
+                                width: 150,
+                                display: "grid",
+                                gridTemplateRows: "35px 35px 35px 35px",
+                                color: hoverMenu === '4' ? 'black' : '#A5A5A5'
+                            }}
+                        >
+              <div className="menu-item">온라인문의</div>
+              <div className="menu-item">채용안내</div>
+            </span>
+                    </div>
+                    <div/>
                 </div>
-                <div/>
             </div>
-        </div>
-    </>
+        </>
+    );
 }
