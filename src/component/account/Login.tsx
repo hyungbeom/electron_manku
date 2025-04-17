218d931dce68db48d425218edaa0b31724f5b98f
12gina <kjh@progist.co.kr>
2025-04-17 15:35:06 +0900

    [fixed] 견적의뢰 ref => useState로 1차 수정

--- src/component/account/Login.tsx
+++ src/component/account/Login.tsx
@@ -49,7 +49,7 @@ export default function Login() {
         }
     }
 
-    const [adminLogin, setAdminLogin] = useState(false);
+    const [adminLogin, setAdminLogin] = useState(true);
 
     return <>
         <Button style={{borderRadius : 5}} onClick={()=>{
@@ -62,10 +62,10 @@ export default function Login() {
                       style={{borderRadius: 5}}
                       placeholder={'input your password'}/>
             <div style={{textAlign: 'left'}}>
-                <Checkbox style={{color: 'gray'}}>아이디저장</Checkbox>
+                <Checkbox style={{color: 'white', fontSize : 12}}>아이디저장</Checkbox>
             </div>
             <Button type={'primary'} style={{height: '100%', borderRadius: 5}} onClick={getLogin}>LOGIN</Button>
-            <div style={{textAlign: 'center'}}>or</div>
+            <div style={{textAlign: 'center',color: 'white'}}>or</div>
         </> : <></>}
         <LoginButton/>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,73 +35,91 @@ export default function Home(props) {
 
     }, [])
     return (
+        <>
+            <video style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+                zIndex: -1
+            }}
+                   autoPlay
+                   muted
+                   loop
+                   playsInline
+            >
+                <source src="/login_back.mp4" type="video/mp4"/>
+                브라우저가 비디오 태그를 지원하지 않습니다.
+            </video>
+            <div className={'container'} style={{paddingTop : 150}}>
+                <div style={{marginTop: 100, width: 330, margin: '0 auto', backgroundColor: 'rgba(0, 0, 0, 0.5)',  padding : 20, borderRadius : 10}}>
 
-        <div className={'container'}>
-            <div style={{paddingTop: 100, width: 330, margin: '0 auto'}}>
-
-                <div style={{textAlign: 'center'}}>
-                    <div><img src={'/manku_logo.png'} alt="" style={{width: 80}}/><span
-                        style={{fontSize: 33, fontWeight: 300}}>
+                    <div style={{textAlign: 'center'}}>
+                        <div><span
+                            style={{fontSize: 33, fontWeight: 500, color : 'white'}}>
 
                     MANKU ERP</span></div>
 
 
-                    <div style={{fontSize: 14, color: '#000000', opacity: 0.6}}>manku erp will take care of your
-                        convenience
+                        <div style={{fontSize: 14,  opacity: 0.8,color : 'white'}}>manku erp will take care of your
+                            convenience
+                        </div>
+                    </div>
+
+
+                    <div style={{
+                        display: 'grid',
+                        gridTemplateRows: `repeat(${page === 'login' ? 5 : 10}, 40px)`,
+                        rowGap: 20,
+                        textAlign: 'center',
+                        paddingTop: 60
+                    }}>
+
+                        <Tabs defaultActiveKey="login" centered={true} style={{color : 'white'}} items={[
+                            {
+                                key: 'login',
+                                label: 'LOGIN',
+                            },
+                            {
+                                key: 'signup',
+                                label: ' SIGN UP',
+                            }
+                        ]} onChange={pageChange}/>
+
+
+                        {page === 'login' ? <Login/> : <SignUp/>}
+
+
+                    </div>
+
+
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'flex-end',
+                        textAlign: 'center',
+                        alignItems: 'center'
+                    }}>
+                        <div style={{position: 'absolute', bottom: 30}}>
+                            <div style={{color: 'gray'}}>Manku ERP program <img src="/manku_logo.png" alt=""
+                                                                                width={30}/> Manku Trading
+                            </div>
+                            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                <svg viewBox="64 64 896 896" focusable="false" data-icon="copyright" width="1em"
+                                     height="1em" fill="currentColor" aria-hidden="true">
+                                    <path
+                                        d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372zm5.6-532.7c53 0 89 33.8 93 83.4.3 4.2 3.8 7.4 8 7.4h56.7c2.6 0 4.7-2.1 4.7-4.7 0-86.7-68.4-147.4-162.7-147.4C407.4 290 344 364.2 344 486.8v52.3C344 660.8 407.4 734 517.3 734c94 0 162.7-58.8 162.7-141.4 0-2.6-2.1-4.7-4.7-4.7h-56.8c-4.2 0-7.6 3.2-8 7.3-4.2 46.1-40.1 77.8-93 77.8-65.3 0-102.1-47.9-102.1-133.6v-52.6c.1-87 37-135.5 102.2-135.5z"></path>
+                                </svg>
+                                &nbsp; Powered by manku trading
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-
-                <div style={{
-                    display: 'grid',
-                    gridTemplateRows: `repeat(${page === 'login' ? 5 : 10}, 40px)`,
-                    rowGap: 20,
-                    textAlign: 'center',
-                    paddingTop: 60
-                }}>
-
-                    <Tabs defaultActiveKey="login" centered={true} items={[
-                        {
-                            key: 'login',
-                            label: 'LOGIN',
-                        },
-                        {
-                            key: 'signup',
-                            label: ' SIGN UP',
-                        }
-                    ]} onChange={pageChange}/>
-
-
-                    {page === 'login' ? <Login/> : <SignUp/>}
-
-
-                </div>
-
-
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'flex-end',
-                    textAlign: 'center',
-                    alignItems: 'center'
-                }}>
-                    <div style={{position: 'absolute', bottom: 30}}>
-                        <div style={{color: 'gray'}}>Manku ERP program <img src="/manku_logo.png" alt=""
-                                                                            width={30}/> Manku Trading
-                        </div>
-                        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                            <svg viewBox="64 64 896 896" focusable="false" data-icon="copyright" width="1em"
-                                 height="1em" fill="currentColor" aria-hidden="true">
-                                <path
-                                    d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372zm5.6-532.7c53 0 89 33.8 93 83.4.3 4.2 3.8 7.4 8 7.4h56.7c2.6 0 4.7-2.1 4.7-4.7 0-86.7-68.4-147.4-162.7-147.4C407.4 290 344 364.2 344 486.8v52.3C344 660.8 407.4 734 517.3 734c94 0 162.7-58.8 162.7-141.4 0-2.6-2.1-4.7-4.7-4.7h-56.8c-4.2 0-7.6 3.2-8 7.3-4.2 46.1-40.1 77.8-93 77.8-65.3 0-102.1-47.9-102.1-133.6v-52.6c.1-87 37-135.5 102.2-135.5z"></path>
-                            </svg>
-                            &nbsp; Powered by manku trading
-                        </div>
-                    </div>
-                </div>
             </div>
-
-        </div>
+        </>
     );
 }
 
