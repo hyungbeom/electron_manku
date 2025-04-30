4fec312c5bfc0a9b0d2ab41b9402221ea76a939e
hyungbeom <hblee@progist.co.kr>
2025-04-30 12:24:04 +0900

    a

--- src/manage/function/api.ts
+++ src/manage/function/api.ts
@@ -4,8 +4,8 @@ import https from 'https';
 
 
 //DEV
-// export const API_URL = 'https://manku.progist.co.kr/api';
-export const API_URL = 'http://175.125.92.183:8080/api';
+export const API_URL = 'https://manku.progist.co.kr/api';
+// export const API_URL = 'http://175.125.92.183:8080/api';
 // export const API_URL = 'http://localhost:8080/api';
 
 
