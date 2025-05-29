8f36cba9b4251a90684c20980e13b8dc4ea1374e
hyungbeom <hblee@progist.co.kr>
2025-05-29 20:14:24 +0900

    as

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,15 +8,16 @@ module.exports = {
                 PORT: 3000,
                 NODE_ENV: "production",
             },
-        },
-        {
-            name: "nextjs-apps",
-            script: "node_modules/next/dist/bin/next",
-            args: "start",
-            env: {
-                PORT: 3001,
-                NODE_ENV: "production",
-            },
-        },
+        }
+        // ,
+        // {
+        //     name: "nextjs-apps",
+        //     script: "node_modules/next/dist/bin/next",
+        //     args: "start",
+        //     env: {
+        //         PORT: 3001,
+        //         NODE_ENV: "production",
+        //     },
+        // },
     ],
 };
